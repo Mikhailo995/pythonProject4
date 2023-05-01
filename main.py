6afc1a1f6caea56cd6437be4ad447e836ad2dbdf
kir995 <kir995@ukr.net>
2023-05-01 18:07:50 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import socket
+
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(('127.0.0.1',2000))
+server.listen(4)
+print('working...')
+client_socket, address = server.accept()
+data = client_socket.recv(1024).decode('utf-8')
+print(data)
+content = 'well done, buddy...'.encode('utf-8')
+client_socket.send(content)
+print('shutdown this shit...')
+
+
